d7fca98ed1a45d77ee2a6e5d027ef4b58d4f4598
Alwin Showi <69798846+alw8n@users.noreply.github.com>
2025-03-16 00:47:04 +0530

    Added CORS support

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 import math
 
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/scrape', methods=['POST'])
 def scrape():
     data = request.json
